81d0d9c8290cb678d242071d12433f3d3ee9325c
Nathan Pawloski <natepawloski@gmail.com>
2017-10-27 10:38:04 -0400

    added item and list classes

---
+++ ToDo/Models/List.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class List
+    {
+        [Key]
+        public int ID { get; set; }
+        public string ListNAme { get; set; }
+
+        public virtual ICollection<Item> Item { get; set; }
+
+
+    }
+}
